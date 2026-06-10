ca4e46b9019d8878fe357dbada2dc8b64d5d9a48
Andre Fontoura <andre_fontoura@econeteditora.com.br>
2026-06-10 10:27:29 -0300

    #commit/25 adiciona variavel no docker compose

--- docker/php/start.sh
+++ docker/php/start.sh
@@ -23,8 +23,8 @@ fi
 # Roda as migrations (opcional - descomente se quiser rodar sempre)
 php artisan migrate
 php artisan db:seed
-
-echo "🎯 Iniciando servidor Laravel na porta 8201..."
-php artisan serve --host=0.0.0.0 --port=8201
-
 php artisan storage:link
+
+PORT="${LARAVEL_PORT:-8201}"
+echo "🎯 Iniciando servidor Laravel na porta ${PORT}..."
+php artisan serve --host=0.0.0.0 --port="${PORT}"
